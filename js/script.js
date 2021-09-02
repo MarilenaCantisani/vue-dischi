31b479c6efb66b460ec39a8d714ef8981ee933a3
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-02 15:19:51 +0200

    Added index.html file / style.css file / script.js file with Vue initialization

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("Vue ok", Vue);
+Vue.config.devtools = true;
+
+//// Initialization of Vue 
+const app = new Vue({
+    el: "#root",
+    data: {
+
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+})
