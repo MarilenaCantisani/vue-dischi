50fc5e0a8d722130bf7e195bc15277406d6cdf1e
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-02 16:44:30 +0200

    Added the array of elements with axios and inserted all data in html tags with a cycle for

--- js/script.js
+++ js/script.js
@@ -5,12 +5,20 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: "#root",
     data: {
+        cards: [],
+
 
     },
     methods: {
+
 
     },
     computed: {
 
     },
+    created() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+            this.cards = (res.data.response);
+        });
+    }
 })
